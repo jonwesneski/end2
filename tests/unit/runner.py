5a47c4870e7f591470ee22420ebb10c4722f5c68
Jon Wesneski <jon.wesneski@clearllc.onmicrosoft.com>
2022-03-23 14:51:27 -0500

    trying asyncio.run

--- tests/unit/runner.py
+++ tests/unit/runner.py
@@ -55,7 +55,7 @@ class TestRunMethodAsync(unittest.TestCase):
         async def test_1():
             await asyncio.sleep(0.1)
             assert True
-        result = self.loop.run_until_complete(runner.run_async_test_func(empty_logger, test_1))
+        result = asyncio.run(runner.run_async_test_func(empty_logger, test_1))
         self.assertEqual(result.status, Status.PASSED)
         self.assertEqual(result.message, "")
         self.assertIsNotNone(result.end_time)
@@ -64,7 +64,7 @@ class TestRunMethodAsync(unittest.TestCase):
         async def test_2(a):
             await asyncio.sleep(0.1)
             assert False
-        result = self.loop.run_until_complete(runner.run_async_test_func(empty_logger, test_2, 1))
+        result = asyncio.run(runner.run_async_test_func(empty_logger, test_2, 1))
         self.assertEqual(result.status, Status.FAILED)
         self.assertNotEqual(result.message, "")
         self.assertIsNotNone(result.end_time)
@@ -73,7 +73,7 @@ class TestRunMethodAsync(unittest.TestCase):
         async def test_3(a, b):
             await asyncio.sleep(0.1)
             raise exceptions.SkipTestException("I skip")
-        result = self.loop.run_until_complete(runner.run_async_test_func(empty_logger, test_3, a=1, b=2))
+        result = asyncio.run(runner.run_async_test_func(empty_logger, test_3, a=1, b=2))
         self.assertEqual(result.status, Status.SKIPPED) and self.assertEqual(result.message, "I skip") 
         self.assertIsNotNone(result.end_time)
 
@@ -83,7 +83,7 @@ class TestRunMethodAsync(unittest.TestCase):
             raise exceptions.IgnoreTestException("Error")
 
         def run_to_completion():
-            return self.loop.run_until_complete(runner.run_async_test_func(empty_logger, test_4))
+            return asyncio.run(runner.run_async_test_func(empty_logger, test_4))
 
         self.assertRaises(exceptions.IgnoreTestException, run_to_completion)
         
@@ -91,7 +91,7 @@ class TestRunMethodAsync(unittest.TestCase):
         async def test_4(a, b, c):
             await asyncio.sleep(0.1)
             raise Exception("Error")
-        result = self.loop.run_until_complete(runner.run_async_test_func(empty_logger, test_4, 1, 2, 3))
+        result = asyncio.run(runner.run_async_test_func(empty_logger, test_4, 1, 2, 3))
         self.assertEqual(result.status, Status.FAILED)
         self.assertIn("Encountered an exception", result.message)
         self.assertIsNotNone(result.end_time)
