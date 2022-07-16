fb1e980ada20e82d8dd37abcaab061a5765b0104
Jon Wesneski <jon.wesneski@clearllc.onmicrosoft.com>
2022-07-16 18:04:16 -0500

    updated unit tests

--- end2/pattern_matchers/base.py
+++ end2/pattern_matchers/base.py
@@ -1,23 +1,25 @@
+from typing import List
+
 
 class PatternMatcherBase:
-    def __init__(self, items: list, pattern: str, include: bool):
+    def __init__(self, items: List[str], pattern: str, include: bool):
         self._items = items
         self._pattern = pattern
         self._include = include
 
     @classmethod
     def parse_str(cls, string: str, include: bool = True):
-        return cls(string, include)
+        return cls([], string, include)
 
     def __str__(self) -> str:
         return f"{'include' if self._include else 'exclude'}: {self._items}"
 
     @property
-    def included_items(self) -> list:
+    def included_items(self) -> List[str]:
         return self._items if self._include else []
 
     @property
-    def excluded_items(self) -> list:
+    def excluded_items(self) -> List[str]:
         return self._items if not self._include else []
 
     def included(self, item: str) -> bool:
