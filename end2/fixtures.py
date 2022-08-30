032cd1487536d604c1e03b638fb7752a99a6592d
Jon Wesneski <jon.wesneski@clearllc.onmicrosoft.com>
2022-08-30 16:21:34 -0500

    cleaned up fixtures

--- end2/fixtures.py
+++ end2/fixtures.py
@@ -1,25 +1,17 @@
-import asyncio
 import functools
-from inspect import iscoroutine
 
 from end2.constants import FUNCTION_TYPE
 from end2.exceptions import MoreThan1FixtureException
 
 
 def setup_module(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
-    wrapper.setup_module = None
-    return wrapper
+    func.setup_module = None
+    return func
 
 
 def teardown_module(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
-    wrapper.teardown_module = None
-    return wrapper
+    func.teardown_module = None
+    return func
 
 
 def on_failures_in_module(func):
@@ -41,44 +33,28 @@ def on_test_failure(func):
 
 
 def setup(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
-    wrapper.setup = None
-    return wrapper
+    func.setup = None
+    return func
 
 
 def setup_test(func):
-    # @functools.wraps(func)
-    # def wrapper(*args, **kwargs):
-    #     return func(*args, **kwargs)
-    # wrapper.setup_test = None
     func.setup_test = None
     return func
 
 
 def teardown_test(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
-    wrapper.teardown_test = None
-    return wrapper
+    func.teardown_test = None
+    return func
 
 
 def teardown(func):
-    # @functools.wraps(func)
-    # def wrapper(*args, **kwargs):
-    #     return func(*args, **kwargs)
     func.teardown = None
     return func
 
 
 def package_test_parameters(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
-    wrapper.package_test_parameters = None
-    return wrapper
+    func.package_test_parameters = None
+    return func
 
 
 def metadata(**kwargs):
@@ -100,11 +76,11 @@ def parameterize(parameters_list: list, first_arg_is_name: bool = False):
     return wrapper
 
 
-def empty_func(*args, **kwargs) -> None:
+def _empty_func(*args, **kwargs) -> None:
     return
 
 
-def get_fixture(module, name: str, default=empty_func):
+def get_fixture(module, name: str, default=_empty_func):
     fixture = default
     found = False
     for key in dir(module):
